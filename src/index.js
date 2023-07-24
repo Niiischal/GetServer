b4bcfc1f5e01690de902bce16772783e7caf989b
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-24 08:34:48 +0545

    installing mongoose

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app= express()
 const port= 5000
 
+const data=[{
+    id:1,
+    firstName:'Nischal',
+    lastName:'Khatiwada',
+},
+
+{
+    id:2,
+    firstName:'Aryan',
+    lastName:'Dhakal',
+}
+
+]
 app.get('/', (req, res)=>{
-    res.send('API is running')
+    res.send(data)
 })
 
 app.listen(port, () => {
