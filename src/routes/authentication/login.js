3cf494d36267cea81d42b1b2b0940f1ff1645fe3
Niiischal <nischal.khatiwada745@gmail.com>
2023-08-08 16:39:04 +0545

    created an api for login

--- src/routes/authentication/login.js
+++ src/routes/authentication/login.js
@@ -1,35 +1,39 @@
-const router = require('express').Router();
-const {User, validateLogin} = require('../../models/Users');
-const bcrypt = require('bcrypt');
+const router = require("express").Router();
+const { User } = require("../../models/Users");
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
 
-router.post('/', async (req, res) => {
-    let body = {...req.body}
-    try{
-        const {error} = validateLogin(body);
-        if(error){
-            return res.status(400).send({message: error.details[0].message});
-    }
+router.post("/", async (req, res) => {
+	try {
+		const { error } = validate(req.body);
+		if (error)
+			return res.status(400).send({ message: error.details[0].message });
 
-    if(!body.role) body = {...body, role:'user'}
+		const user = await User.findOne({ username: req.body.username });
+		if (!user)
+			return res.status(401).send({ message: "Invalid Username or Password" });
 
-    const {username, password} = req.body
-    const user = await User.findOne({username});
-    if(!user){
-        return res.status(409).send({message: 'Please login with correct username.'})
-    }
-    //compare password hash from database and entered plain text password using compare method of bcrytjs
-    const validPassword =  await bcrypt.compare(password, user.hashedPassword
-        );
-        console.log("valid Password", validPassword )
-        if (!validPassword){
-            return res.status(401).send({'message': "Invalid credentials"});
-        }
-        else{
-            res.status(201).send({message:'User logged in  successfully.'});
-    }
-    }catch(err){
-        res.status(500).send({message:'Internal server error'});
-    }
-})
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		if (!validPassword)
+			return res.status(401).send({ message: "Invalid Email or Password" });
+
+		const token = user.generateAuthToken();
+		
+		return res.status(200).send({ token, message: "logged in successfully" });
+	} catch (error) {
+		return res.status(500).send({ message: "Internal Server Error", error });
+	}
+});
+
+const validate = (data) => {
+	const schema = Joi.object({
+		username: Joi.string().username().required().label("Username"),
+		password: Joi.string().required().label("Password"),
+	});
+	return schema.validate(data);
+};
 
 module.exports = router;
